5a606f4f92bca051011a0e3cbd77cff3642fee4f
root <wkdwodud07@kaist.ac.kr>
2019-03-06 22:42:32 +0900

    ADD : make sleep thread list

--- src/threads/thread.c
+++ src/threads/thread.c
@@ -28,6 +28,7 @@
 /* List of processes in THREAD_READY state, that is, processes
    that are ready to run but not actually running. */
 static struct list ready_list;
+static struct list sleep_list;
 
 /* Idle thread. */
 static struct thread *idle_thread;
@@ -95,6 +96,7 @@ thread_init (void)
 
   lock_init (&tid_lock);
   list_init (&ready_list);
+  list_init (&sleep_list);
 
   /* Set up a thread structure for the running thread. */
   initial_thread = running_thread ();
