9e99ee3b76959865f7b418d0dd7a06f5e71d8eae
root <wkdwodud07@kaist.ac.kr>
2019-03-09 17:45:30 +0900

    TEST: is thread.c is saved?

--- src/threads/thread.c
+++ src/threads/thread.c
@@ -239,6 +239,9 @@ thread_unblock (struct thread *t)
 
   old_level = intr_disable ();
   ASSERT (t->status == THREAD_BLOCKED);
+
+  // When we try to push the thread to the read_list, we should consider the priority.
+  // list_insert_ordered();
   list_push_back (&ready_list, &t->elem);
   t->status = THREAD_READY;
   intr_set_level (old_level);
