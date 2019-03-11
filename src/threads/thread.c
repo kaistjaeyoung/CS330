a78293cceb2d63e3d8fcacc5503fdb7927fc9a00
root <wkdwodud07@kaist.ac.kr>
2019-03-12 01:54:51 +0900

    initialize thread holding_lock_list

--- src/threads/thread.c
+++ src/threads/thread.c
@@ -492,6 +492,7 @@ init_thread (struct thread *t, const char *name, int priority)
   t->priority = priority;
   t->magic = THREAD_MAGIC;
   t->original_priority = priority;
+  list_init(&t->holding_lock_list);
 }
 
 /* Allocates a SIZE-byte frame at the top of thread T's stack and
