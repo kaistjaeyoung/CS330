f05971fbd2c777dd540dd07f905c86cb979169f0
root <wkdwodud07@kaist.ac.kr>
2019-03-19 15:34:16 +0900

    Reflect changed Makefile.userprog and p_vaddr

--- src/userprog/process.c
+++ src/userprog/process.c
@@ -459,7 +459,7 @@ validate_segment (const struct Elf32_Phdr *phdr, struct file *file)
      it then user code that passed a null pointer to system calls
      could quite likely panic the kernel by way of null pointer
      assertions in memcpy(), etc. */
-  if (phdr->p_offset < PGSIZE)
+  if (phdr->p_vaddr < PGSIZE)
     return false;
 
   /* It's okay. */
